24e55a85c7d54647b484f18d8f3a2d2fa04008c5
Robin Kim <therobinkim@gmail.com>
2014-09-02 15:18:04 -0700

    srs.js: complete Topic

--- public/client/library/srs.js
+++ public/client/library/srs.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // "snooze" each card
 // get it right? long snooze! :)
 // get it wrong? short snooze. :(
@@ -49,18 +51,64 @@ deck2.addCard(card1);
 // deck 1 and deck 2 both have card 1
 // card 1 belongs to both deck 1 and deck 2
 
-var Topic = function() {
+
+var Topic = function(topicName) {
+  var decks = [];
+  for(var i = 1; i < arguments.length; i++) {
+    decks.push(arguments[i]);
+  }
+
   return {
-    _decks: [],
-    name: '',
+    _decks: decks,
+    name: topicName || '',
 
-    addDeck: function() {},
-    removeDeck: function() {},
-    getDeck: function() {},
+    // precondition: pass in new instance of Deck
+    // postcondition: if Topic doesn't have Deck, add it.
+    //   otherwise, don't add it.
+    addDeck: function(deck) {
+      if(this.isDeck(deck.getName())) {
+        return null;
+      } else {
+        this._decks.push(deck);
+      }
+    },
 
-    setName: function() {},
-    getName: function() {}
+    removeDeck: function(deckName) {
+      for(var i = 0; i < this._decks.length; i++) {
+        if(this._decks[i].getName() === deckName) {
+          this._decks.splice(i, 1);
+        }
+      }
+    },
 
+    getDeck: function(deckName) {
+      for(var i = 0; i < this._decks.length; i++) {
+        if(this._decks[i].getName() === deckName) {
+          this._decks.splice(i, 1);
+        }
+      }
+    },
+
+    isDeck: function(deckName) {
+      for(var i = 0; i < this._decks.length; i++) {
+        if(this._decks[i].getName() === deckName) {
+          return true;
+        }
+      }
+      return false;
+    },
+
+    setName: function(topicName) {
+      this.name = topicName;
+    },
+
+    getName: function() {
+      return this.name;
+    },
+
+    isName: function(topicName){
+      return this.getName() === topicName;
+    }
   };
 };
 
