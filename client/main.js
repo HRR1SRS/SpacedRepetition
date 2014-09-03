89d4e365e5775d9af4d5e04de26ba356257ad66a
vagrant <vagrant@precise32.(none)>
2014-09-03 09:18:21 +0000

    convert to Meteor app

---
+++ client/main.js
@@ -0,0 +1,11 @@
+if (Meteor.isClient) {
+  console.log('hi client');
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    console.log('hi server 1');
+  });
+  console.log('hi server 2');
+}
