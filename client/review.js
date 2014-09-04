fefc4d66fe1ebd55a8f9ef01273fb075cbeb0a66
Robin Kim <therobinkim@gmail.com>
2014-09-04 05:22:01 +0000

    fix minor linting issues and bug

--- client/review.js
+++ client/review.js
@@ -1,39 +1,39 @@
 if(Meteor.isClient){
   Template.review.topicList = function(){
-  	var topics = [{name:"Javascript"}, {name: "Spanish"}, {name:"Human Anatomy"}, {name:"Russian Literature"}];
-  	return topics;
-  }
-	var clickedTopic = {};
+    var topics = [{name:'Javascript'}, {name: 'Spanish'}, {name:'Human Anatomy'}, {name:'Russian Literature'}];
+    return topics;
+  };
+  var clickedTopic = {};
 
   Template.review.selectedTopics = function(name){
-  }
+  };
 
   Template.review.card = function(arg){
-  	var cardData = {question: "Who Shot Mr. Burns?", answer: "Maggie"}
-  	if(arg === undefined){
-			console.log(cardData.question);
-  		return cardData.question;
-  	}else{
-  		console.log(cardData.answer)
-  		return cardData.answer;
-  	}
+    var cardData = {question: 'Who Shot Mr. Burns?', answer: 'Maggie'};
+    if(arg === undefined){
+      console.log(cardData.question);
+      return cardData.question;
+    }else{
+      console.log(cardData.answer);
+      return cardData.answer;
+    }
 
-  } 
+  } ;
 
   Template.review.events({
-  	'click select option': function(){
-  		var name = this.name.toLowerCase().split(" ").join("");
-  		if(!clickedTopic[this.name]){
-  			clickedTopic[this.name] = name;
-   			$(".selectedTopics ul").append("<li id='"+name+"'>"+this.name+"</li>");
-  		}else{
-				$("#"+name).remove();
-				delete clickedTopic[this.name];  			
+    'click select option': function(){
+      var name = this.name.toLowerCase().split(' ').join('');
+      if(!clickedTopic[this.name]){
+        clickedTopic[this.name] = name;
+        $('.selectedTopics ul').append('<li id="+name+">'+this.name+'</li>');
+      }else{
+        $('#'+name).remove();
+        delete clickedTopic[this.name];       
 
-  		}
-   	},
-   	'click .button': function(){
-   		Template.review.card(true);
-   	}
+      }
+    },
+    'click .button': function(){
+      Template.review.card(true);
+    }
   });
 }
