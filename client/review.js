e4cd5aa2c1bd0e73ba937cf2a0fab918b1ace4c9
Mike Axtman <mdaxtman@gmail.com>
2014-09-04 11:59:31 -0700

    added functionality to view answer, added buttons to rate question

--- client/review.js
+++ client/review.js
@@ -1,39 +1,39 @@
 if(Meteor.isClient){
-  Template.review.topicList = function(){
+  Template.review.helpers({
+    //displays card question and answer
+    card : function(arg){
+      var cardData = {question: 'Who Shot Mr. Burns?', answer: 'Maggie'};
+      if(arg === undefined){
+        return cardData.question;
+      }else{
+        $('.card p').after('<p class="answer"><b>'+cardData.answer+'</b></p>')
+        $('.answer').after('<br><p>rate your difficulty with the question</p><button>easy</button><button>hard</button>')
+        $('.button').remove();
+      }
+    },
+    //displays lists of topics available
+    topicList : function(){
     var topics = [{name:'Javascript'}, {name: 'Spanish'}, {name:'Human Anatomy'}, {name:'Russian Literature'}];
     return topics;
-  };
-  var clickedTopic = {};
-
-  Template.review.selectedTopics = function(name){
-  };
-
-  Template.review.card = function(arg){
-    var cardData = {question: 'Who Shot Mr. Burns?', answer: 'Maggie'};
-    if(arg === undefined){
-      console.log(cardData.question);
-      return cardData.question;
-    }else{
-      console.log(cardData.answer);
-      return cardData.answer;
     }
 
-  } ;
-
+  })
+    //click event that lists topics being reviewed
+  var clickedTopic = {};
   Template.review.events({
     'click select option': function(){
       var name = this.name.toLowerCase().split(' ').join('');
       if(!clickedTopic[this.name]){
         clickedTopic[this.name] = name;
-        $('.selectedTopics ul').append('<li id="+name+">'+this.name+'</li>');
+        $('.selectedTopics ul').append('<li id="'+name+'">'+this.name+'</li>');
       }else{
         $('#'+name).remove();
         delete clickedTopic[this.name];       
-
       }
     },
+    //button to reveal topic
     'click .button': function(){
-      Template.review.card(true);
+      Template.review.cardQuestion(true);
     }
   });
 }
