b1b8ee3bc854f84bca2a8aab1e7f1c20334e628b
ikedi <icukaegbu@gmail.com>
2014-09-05 11:22:14 +0100

    Refactored addCard.html to call cardItem template; moved logic to cardItem.js

--- client/addCard.js
+++ client/addCard.js
@@ -1,30 +1,36 @@
-Template.addCard.events({
-  'submit': function(event, template) {
-    var e = event;
-    var t = template;
-    e.preventDefault();
-    var topic = t.find('#topic').value;
-    var question = t.find('#question').value;
-    var answer = t.find('#answer').value;
+// Template.addCard.events({
+//   'submit': function(event, template) {
+//     var e = event;
+//     var t = template;
+//     e.preventDefault();
+//     var topic = t.find('#topic').value;
+//     var question = t.find('#question').value;
+//     var answer = t.find('#answer').value;
 
-    // first create card
-    var newCard = Cards.insert({
-      question: question,
-      answer: answer
-    });
+//     // first create card
+//     var newCard = Cards.insert({
+//       question: question,
+//       answer: answer
+//     });
 
-    // if we find the topic
-    if(Topics.find({name: topic}).fetch().length) {
-      // update topic to have newCard
-      var topicId = Topics.findOne({name: topic});
-      Topics.update({_id: topicId._id}, {$push: {cards: newCard}});
-      Cards.update({_id: newCard}, {$set: {topic: topicId._id}});
-    }
-    // else it doesn't exist, so create it
-    else {
-      var newTopic = Topics.insert({name: topic, cards: [newCard]});
-      // update newCard to have newTopic
-      Cards.update({_id: newCard}, {$set: {topic: newTopic}});
-    }
-  }
-});
+//     // if we find the topic
+//     // if(Topics.find({name: topic}).fetch().length) {
+//     //   // update topic to have newCard
+//     //   var topicId = Topics.findOne({name: topic});
+//     //   Topics.update({_id: topicId._id}, {$push: {cards: newCard}});
+//     //   Cards.update({_id: newCard}, {$set: {topic: topicId._id}});
+//     // }
+//     var topicId = Topics.findOne({name: topic});
+//     if(topicId) {
+//       // update topic to have newCard
+//       Topics.update({_id: topicId._id}, {$push: {cards: newCard}});
+//       Cards.update({_id: newCard}, {$set: {topic: topicId._id}});
+//     }
+//     // else it doesn't exist, so create it
+//     else {
+//       var newTopic = Topics.insert({name: topic, cards: [newCard]});
+//       // update newCard to have newTopic
+//       Cards.update({_id: newCard}, {$set: {topic: newTopic}});
+//     }
+//   }
+// });
--- client/templates/cards/card_item.js
+++ client/templates/cards/card_item.js
@@ -3,3 +3,47 @@
 //     return this;
 //   }
 // })
+
+Template.cardItem.events({
+  'submit': function(event, template) {
+    var e = event;
+    var t = template;
+    e.preventDefault();
+    var topic = t.find('#topics').value;
+    var question = t.find('#question').value;
+    var answer = t.find('#answer').value;
+
+    // first create card
+    var newCard = Cards.insert({
+      question: question,
+      answer: answer
+    });
+
+    var topicId = Topics.findOne({name: topic});
+    if(topicId) {
+      // update topic to have newCard
+      Topics.update({_id: topicId._id}, {$push: {cards: newCard}});
+      Cards.update({_id: newCard}, {$set: {topic: topicId._id}});
+
+      //save was successful, so display growl and clear textfield
+      //clear(t);
+
+      
+    }
+    // else it doesn't exist, so create it
+    else {
+      var newTopic = Topics.insert({name: topic, cards: [newCard]});
+      // update newCard to have newTopic
+      Cards.update({_id: newCard}, {$set: {topic: newTopic}});
+    }
+
+  }
+});
+
+// Template.cardItem.clear = function(template){
+// 	var t = template;
+
+// 	t.find('#topics').value = '';
+// 	t.find('#question').value = '';
+// 	t.find('#tanswer').value = '';
+// }
