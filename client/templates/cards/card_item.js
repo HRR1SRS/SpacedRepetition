8a6c6feb3daaab19ba3e798f19913547e3809caf
ikedi <icukaegbu@gmail.com>
2014-09-05 13:00:15 +0100

    Refactored card_item.js to prevent save on empty Q/A fields

--- client/templates/cards/card_item.js
+++ client/templates/cards/card_item.js
@@ -13,33 +13,57 @@ Template.cardItem.events({
     var question = t.find('#question').value;
     var answer = t.find('#answer').value;
 
-    // first create card
-    var newCard = Cards.insert({
-      question: question,
-      answer: answer
-    });
+    var newCard = null;
 
-    var topicId = Topics.findOne({name: topic});
-    if(topicId) {
-      // update topic to have newCard
-      Topics.update({_id: topicId._id}, {$push: {cards: newCard}});
-      Cards.update({_id: newCard}, {$set: {topic: topicId._id}});
+    //test for successful save
+    var isSave = false;
 
+    //only create card if fields are not blank
+    if ( question && answer ){
+      //console.log(question + ': '+answer);
+      // first create card
+      newCard = Cards.insert({
+        question: question,
+        answer: answer
+      });
+    }else{
+      //console.log('fields r blank');
+      isSave = false;
+    }
+
+    if ( newCard ) {
+      //only create topic if field is not blank
+      var topicId = topic !== null ? Topics.findOne({name: topic}) : null;
+
+      if( topicId ) {
+        // update topic to have newCard
+        Topics.update({_id: topicId._id}, {$push: {cards: newCard}});
+        Cards.update({_id: newCard}, {$set: {topic: topicId._id}});
+      }
+      // else it doesn't exist, so create it
+      else {
+        var newTopic = Topics.insert({name: topic, cards: [newCard]});
+        // update newCard to have newTopic
+        Cards.update({_id: newCard}, {$set: {topic: newTopic}});
+      }
+
+      isSave = true;
+    }
+    
+
+    if (isSave){
       //save was successful, so display growl and clear textfield
       //clear(t);
-
       t.find('#topics').value = '';
-	    t.find('#question').value = '';
-	    t.find('#answer').value = '';
+      t.find('#question').value = '';
+      t.find('#answer').value = '';
 
       //display notification
       Notifications.info('Save Successful', 'Successfully added Question: '+question);
     }
-    // else it doesn't exist, so create it
     else {
-      var newTopic = Topics.insert({name: topic, cards: [newCard]});
-      // update newCard to have newTopic
-      Cards.update({_id: newCard}, {$set: {topic: newTopic}});
+      //display notification
+      Notifications.error('Save Unsuccessful', 'Could not save Question: '+question);
     }
 
   }
